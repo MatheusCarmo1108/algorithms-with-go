bac367cc86af51dfd8c1e863c4484b0d9abefed4
Matheus Carmo <matheuscarmo1108@gmail.com>
2021-04-08 00:08:13 -0300

    Update 1st algorithm

--- module01/find_two_that_sum/main.go
+++ module01/find_two_that_sum/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main(){
+import "fmt"
+
+func FindTwoThatSum(numbers []int, sum int) (int, int) {
+	for i, num1 := range numbers {
+		for j, num2 := range numbers {
+			if i == j {
+				continue
+			}
+			if num1+num2 == sum {
+				return i, j
+			}
+		}
+	}
+	return -1, -1
+}
+
+func main() {
+	slice := []int{1, 2, 3, 4}
+	somat := 7
+
+	fmt.Println(FindTwoThatSum(slice, somat))
 
 }
